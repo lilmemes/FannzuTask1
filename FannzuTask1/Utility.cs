76b40c2251c6148fa09877b82c99b9974323bf7e
Сыв <@DESKTOP-6TOAO0B>
2023-03-03 17:28:54 +0300

    Added Utility class.

---
+++ FannzuTask1/Utility.cs
@@ -0,0 +1,61 @@
+﻿namespace FannzuTask1
+{
+    public class Utility
+    {
+
+        public static int Sum(int s, int a)
+        {
+            return s + a;
+        }
+
+        public static int Minus(int s, int a)
+        {
+            return s - a;
+        }
+
+        public static int Multiply(int s, int a)
+        {
+            return s * a;
+        }
+
+        public static double Divide(double s, double a)
+        {
+            return s / a;
+        }
+
+        public static bool Equal(int s, int a)
+        {
+            return s == a;
+        }
+
+        public static int Pow(int s)
+        {
+            return s * s;
+        }
+
+        public static int Div(int s, int a)
+        {
+            return s % a;
+        }
+
+        public static string FirstCharToUpper(string initialString)
+        {
+            return initialString[0].ToString().ToUpper() + initialString.Substring(1);
+        }
+
+        public static string WholeTextToUpper(string initialString)
+        {
+            return initialString.ToUpper();
+        }
+
+        public static string CustomSubstring(string initialString, int n)
+        {
+            if (initialString.Length < n)
+            {
+                return "POWEL NAHUI";
+            }
+
+            return initialString.Substring(5, n);
+        }
+    }
+}
